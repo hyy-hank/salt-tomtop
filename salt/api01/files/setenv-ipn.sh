df4161fd616e908b11f25d7f484e1c963220529f
root <2853635720@qq.com>
2016-08-23 11:33:14 +0800

    modify test api jvm memory

--- salt/api01/files/setenv-ipn.sh
+++ salt/api01/files/setenv-ipn.sh
@@ -3,7 +3,7 @@ CATALINA_PID="$CATALINA_BASE/tomcat.pid"
 #add java opts
 CATALINA_OPTS="-Djava.library.path=/usr/local/tomcat-native/lib"
 {% if grains['env'] == 'test' %}
-JAVA_OPTS="-server -Xms4096M -Xmx4096M -Dspring.profiles.active=test"
+JAVA_OPTS="-server -Xms1024M -Xmx4096M -Dspring.profiles.active=test"
 {% elif grains['env'] == 'uat' %}
 JAVA_OPTS="-server -Xms4096M -Xmx4096M -Dspring.profiles.active=uat"
 {% elif grains['env'] == 'prod' %}
